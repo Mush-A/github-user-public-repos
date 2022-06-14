7f14f7e4819713d675ef3af1fc8f65e649cc8301
Musharaf Altaf <mushrafaltaf@gmail.com>
2022-06-14 14:18:58 +0530

    Style: RepoCard style updated

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -2,14 +2,37 @@ import "../style/RepoCard.css"
 
 const RepoCard = (props) => {
 
-    const {name, description, updated_at} = props.repo
+    const {name, description, updated_at, language, topics, html_url} = props.repo
     
     return (
         <div className="repo-card">
-            <h3 className="title">{name}</h3>
-            <p className="description">{description}</p>
-            <small className="update-time">{updated_at}</small>
+            <div className="repo-card-header">
+                <p className="repo-name">{name}</p>
+                <p className="repo-lang">{`<${language} />`}</p>
+            </div>
+
+            <p className="repo-description">{description}</p>
+            <div className="repo-topics">
+                {
+                    topics.map(topic => {
+                        return <Topics topic={topic} />
+                    })
+                }
+            </div>
+
+            <div className="repo-card-footer">
+                <small className="repo-update-time">{"Last updated " + Date(updated_at).split(" ").slice(1, 6).join(" ")}</small>
+                <a href={html_url} className="">View Repo</a>
+            </div>
         </div>
+    )
+}
+
+const Topics = ({topic}) => {
+    return (
+        <span className="repo-topic">
+            {topic}
+        </span>
     )
 }
 
